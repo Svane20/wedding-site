1b9ee3b55a7623d5ad823a1ba58e28a400cf543e
Kasper Svane <svane.kasper20@gmail.com>
2024-09-01 23:01:41 +0200

    feature/footer Added footer section

--- src/app/components/app/app.component.ts
+++ src/app/components/app/app.component.ts
@@ -5,11 +5,12 @@ import { CoupleComponent } from '../../../libs/couple/couple.component';
 import { AudioPlayerComponent } from '../../../libs/audio-player/audio-player.component';
 import { StoryComponent } from '../../../libs/story/story.component';
 import { EventComponent } from '../../../libs/event/event.component';
+import {FooterComponent} from "../../../libs/footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, HomeComponent, CoupleComponent, AudioPlayerComponent, StoryComponent, EventComponent],
+  imports: [HeaderComponent, HomeComponent, CoupleComponent, AudioPlayerComponent, StoryComponent, EventComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
